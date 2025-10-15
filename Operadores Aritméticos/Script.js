feda40fd38926f76caf6d36aa12af1d3b24cd8da
Israel Santos <121396259+YsraellSantos@users.noreply.github.com>
2025-10-14 21:39:48 -0300

    Operadores aritiméticos

---
+++ Operadores Aritméticos/Script.js
@@ -0,0 +1,52 @@
+// Operadores aritiméticos
+const operadoresAritiméticos = function(){
+    // Soma
+    let a = 10
+    let b = 20
+    const soma = a + b
+    console.log("Soma")
+    console.log(`A soma entre (${a} + ${b} é = ${soma})`)
+    console.log("--------------")
+    
+    // Subtração
+    let c = 30
+    let d = 25
+    const subtracao = c - d
+    console.log("subtração")
+    console.log(`A subtração entre (${c} - ${d} é = ${subtracao})`)
+    console.log("--------------")
+    
+    // Multiplicação
+    let e = 2
+    let f = 50
+    const multiplicacao = e * f
+    console.log("Multiplicação")
+    console.log(`A multiplicação entre (${e} x ${f} é = ${multiplicacao})`)
+    console.log("---------------")
+    
+    // Divisão
+    let g = 3
+    let h = 90
+    const divisao = h / g
+    console.log("Divisão")
+    console.log(`A divisão entre (${g} / ${h}) é = ${divisao})`)
+    console.log("---------------")
+    
+    // Resto da Divisão
+    let i = 10
+    let j = 3
+    const restoDaDivisao = i % j
+    console.log("Resto da Divisão")
+    console.log(`O resto da divisão entre (${i} % ${j}) é igual á resto ${restoDaDivisao})`)
+    console.log("---------------")
+    
+    // Exponencial
+    let k = 5
+    let l = 3
+    const exponencial = k ** l
+    console.log("Exponencial")
+    console.log(`O valor de (${k} elevado á ${l}) é igual á ${exponencial})`)
+    
+}
+
+operadoresAritiméticos()
